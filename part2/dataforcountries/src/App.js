5a9390239437d3933cab014a7d30c49ef271a82a
Kian <mr.kian.vf@gmail.com>
2022-09-05 14:10:31 +0430

    Add files via upload

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -5,20 +5,39 @@ import CountriesList from "./components/CountriesList";
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [newFilter, setNewFilter] = useState("");
+  const [weather, setWeather] = useState(0);
+  const [capitalInfo, setCapitalInfo] = useState(0);
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
       setCountries(response.data);
+      console.log("fafa");
     });
   }, []);
+
   const handleFilterChange = (event) => {
     setNewFilter(event.target.value);
-    console.log(newFilter);
   };
+
+  const showDetails = (countryName) => {
+    setNewFilter(countryName);
+  };
+  const getWeather = (capitalInfo) => {};
+
+  const api_key = "def5d565b5611b527557b695a2b9a0f5";
 
   return (
     <div>
       <Search newFilter={newFilter} handleFilterChange={handleFilterChange} />
-      <CountriesList countries={countries} newFilter={newFilter} />
+      <CountriesList
+        countries={countries}
+        newFilter={newFilter}
+        showDetails={showDetails}
+        getWeather={getWeather}
+        weather={weather}
+        setWeather={setWeather}
+        api_key={api_key}
+        setCapitalInfo={setCapitalInfo}
+      />
     </div>
   );
 };
--- part2/dataforcountries/src/components/CountriesList.js
+++ part2/dataforcountries/src/components/CountriesList.js
@@ -1,7 +1,31 @@
-const CountriesList = ({ countries, newFilter }) => {
+import axios from "axios";
+import { useEffect } from "react";
+const CountriesList = ({
+  countries,
+  newFilter,
+  showDetails,
+  getWeather,
+  weather,
+  setWeather,
+  api_key,
+  setCapitalInfo,
+}) => {
   const filteredCountries = countries.filter((c) =>
     c.name.common.toLowerCase().includes(newFilter.toLowerCase())
   );
+
+  // useEffect(() => {
+  //   if (filteredCountries !== undefined) {
+  //     axios
+  //       .get(
+  //         `https://api.openweathermap.org/data/2.5/weather?lat=${filteredCountries[0].capitalInfo.latlng[0]}&lon=${filteredCountries[0].capitalInfo.latlng[1]}&appid=${api_key}`
+  //       )
+  //       .then((response) => {
+  //         setWeather(response.data);
+  //       });
+  //   }
+  // }, []);
+
   if (filteredCountries.length > 10) {
     return (
       <div>
@@ -12,15 +36,56 @@ const CountriesList = ({ countries, newFilter }) => {
     return (
       <div>
         {filteredCountries.map((c) => (
-          <p key={c.name.common}>{c.name.common}</p>
+          <Name
+            key={c.name.common}
+            name={c.name.common}
+            showDetails={showDetails}
+          />
         ))}
       </div>
     );
   } else if (filteredCountries.length === 1) {
-    return <Details filteredCountries={filteredCountries} />;
+    return (
+      <Details
+        filteredCountries={filteredCountries}
+        getWeather={getWeather}
+        capitalInfo={filteredCountries[0].capitalInfo}
+        weather={weather}
+        setWeather={setWeather}
+        api_key={api_key}
+      />
+    );
   }
 };
-const Details = ({ filteredCountries }) => {
+
+const Name = ({ name, showDetails }) => {
+  return (
+    <p>
+      {name}
+      <button onClick={() => showDetails(name)}>show</button>
+    </p>
+  );
+};
+
+const Details = ({
+  filteredCountries,
+  getWeather,
+  capitalInfo,
+  weather,
+  setWeather,
+  api_key,
+}) => {
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${capitalInfo.latlng[0]}&lon=${capitalInfo.latlng[1]}&appid=${api_key}&units=metric`
+      )
+      .then((response) => {
+        setWeather(response.data);
+      });
+  }, [capitalInfo.latlng, api_key, setWeather]);
+  // console.log(weather.weather);
+
   return (
     <div>
       <h1>{filteredCountries[0].name.common}</h1>
@@ -37,7 +102,21 @@ const Details = ({ filteredCountries }) => {
         </ul>
         <img src={filteredCountries[0].flags.png} />
       </div>
+      <h2>Weather in {filteredCountries[0].capital}</h2>
+      <Weather country={filteredCountries[0]} weather0={weather} />
     </div>
   );
 };
+const Weather = ({ country, weather0 }) => {
+  if (weather0 !== 0) {
+    return (
+      <div>
+        <p>Temperature: {weather0.main.temp} celsius</p>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+};
+
 export default CountriesList;
