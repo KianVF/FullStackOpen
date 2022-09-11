7372f7527888d0ba21dea71da7c8355b93096d12
Kian <mr.kian.vf@gmail.com>
2022-09-11 10:55:01 +0430

    Add files via upload

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,15 @@
-const { request } = require("express");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 app.use(express.json());
+
+morgan.token("body", (req) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
